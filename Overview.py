6a3cc546f551018201816cfc8f83d6911b8c9b91
Henrik Andersson <jan@dhigroup.com>
2022-08-25 13:52:27 +0200

    Overview

--- Overview.py
+++ Overview.py
@@ -1,5 +1,24 @@
 import streamlit as st
+from scipy.stats import norm, uniform
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
 """
 # Probilistic forecasting in practice
 """
+
+
+x = np.linspace(-5,5,501)
+
+df = pd.DataFrame(dict(x=x))
+df = df.set_index("x")
+
+df['pdf_norm'] = norm.pdf(x)
+df['cdf_norm'] = norm.cdf(x)
+df['pdf_uniform'] = uniform(-2,4).pdf(x)
+df['cdf_uniform'] = uniform(-2,4).cdf(x)
+
+fig = px.line(df)
+
+st.plotly_chart(fig)
